e40737a27d174f4acdab9f47e39a09771737d449
Shahmah <3x_ata@rambler.ru>
2016-02-03 13:48:28 -0600

    adding swiping functionality

--- 4.hybrid-mobile-app-development-frameworks/conFusion/www/js/controllers.js
+++ 4.hybrid-mobile-app-development-frameworks/conFusion/www/js/controllers.js
@@ -50,7 +50,7 @@ angular.module('conFusion.controllers', [])
     $timeout(function() {
       $scope.closeReserve();
     }, 1000);
-  }; 
+  };
 
   // Open the login modal
   $scope.login = function() {
@@ -69,7 +69,7 @@ angular.module('conFusion.controllers', [])
   };
 })
 
-.controller('MenuController', ['$scope', 'menuFactory', 'baseURL', function($scope, menuFactory, baseURL) {
+.controller('MenuController', ['$scope', 'menuFactory', 'favoriteFactory', 'baseURL', '$ionicListDelegate', function($scope, menuFactory, favoriteFactory, baseURL, $ionicListDelegate) {
 
     $scope.baseURL = baseURL
     $scope.tab = 1;
@@ -112,6 +112,12 @@ angular.module('conFusion.controllers', [])
     $scope.toggleDetails = function() {
         $scope.showDetails = !$scope.showDetails;
     };
+
+    $scope.addFavorite = function(index){
+        console.log("index is " + index);
+        favoriteFactory.addToFavorites(index);
+        $ionicListDelegate.closeOptionButtons();
+    }
 }])
 
 .controller('ContactController', ['$scope', function($scope) {
